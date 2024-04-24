1e4a27ddeffeaa4b2fd3fb63e0d1b421c0c04396
Liav Levi <74296214+liavm1998@users.noreply.github.com>
2024-04-24 18:48:54 +0000

    finish calculating final location given epoches or movements

--- ex0/gnss_parser.py
+++ ex0/gnss_parser.py
@@ -158,3 +158,55 @@ sv_position = calculate_satellite_position(ephemeris, one_epoch['tTxSeconds'])
 sv_position.to_csv(os.path.join(parent_directory,'ex0', 'output_xyz.csv'))
 print(sv_position)
 
+
+def least_squares(xs, measured_pseudorange, x0, b0):
+    dx = 100*np.ones(3)
+    b = b0
+    # set up the G matrix with the right dimensions. We will later replace the first 3 columns
+    # note that b here is the clock bias in meters equivalent, so the actual clock bias is b/LIGHTSPEED
+    G = np.ones((measured_pseudorange.size, 4))
+    iterations = 0
+    while np.linalg.norm(dx) > 1e-3:
+        # Eq. (2):
+        r = np.linalg.norm(xs - x0, axis=1)
+        # Eq. (1):
+        phat = r + b0
+        # Eq. (3):
+        deltaP = measured_pseudorange - phat
+        G[:, 0:3] = -(xs - x0) / r[:, None]
+        # Eq. (4):
+        sol = np.linalg.inv(np.transpose(G) @ G) @ np.transpose(G) @ deltaP
+        # Eq. (5):
+        dx = sol[0:3]
+        db = sol[3]
+        x0 = x0 + dx
+        b0 = b0 + db
+    norm_dp = np.linalg.norm(deltaP)
+    return x0, b0, norm_dp
+
+
+b0 = 0
+x0 = np.array([0, 0, 0])
+xs = sv_position[['x_k', 'y_k', 'z_k']].to_numpy()
+x = x0
+b = b0
+ecef_list = []
+for epoch in measurements['Epoch'].unique():
+    one_epoch = measurements.loc[(measurements['Epoch'] == epoch) & (measurements['prSeconds'] < 0.1)] 
+    one_epoch = one_epoch.drop_duplicates(subset='SvName').set_index('SvName')
+    if len(one_epoch.index) > 4:
+        timestamp = one_epoch.iloc[0]['UnixTime'].to_pydatetime(warn=False)
+        sats = one_epoch.index.unique().tolist()
+        ephemeris = manager.get_ephemeris(timestamp, sats)
+        sv_position = calculate_satellite_position(ephemeris, one_epoch['tTxSeconds'])
+
+        xs = sv_position[['x_k', 'y_k', 'z_k']].to_numpy()
+        pr = one_epoch['PrM'] + LIGHTSPEED * sv_position['delT_sv']
+        pr = pr.to_numpy()
+
+        x, b, dp = least_squares(xs, pr, x, b)
+        ecef_list.append(x)
+
+for pos in ecef_list:
+    print(navpy.ecef2lla(pos))
+
